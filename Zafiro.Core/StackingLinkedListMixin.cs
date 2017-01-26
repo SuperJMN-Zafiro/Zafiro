7b46066ae14d22e541f51b82a22906eb807cdb22
José Manuel Nieto Sánchez <superjmn@outlook.com>
2017-01-26 23:27:21 +0100

    First commit

---
+++ Zafiro.Core/StackingLinkedListMixin.cs
@@ -0,0 +1,13 @@
+﻿namespace Glass.Core
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class StackingLinkedListMixin
+    {
+        public static IEnumerable<T> GetAncestors<T>(this StackingLinkedList<T> sll) where T : class
+        {
+            return sll.ToList().Reverse();
+        }
+    }
+}
