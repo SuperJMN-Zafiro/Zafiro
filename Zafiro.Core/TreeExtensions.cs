dedd8d13d9ec0495129701457fa55e7ff8444f0e
José Manuel Nieto Sánchez <superjmn@outlook.com>
2017-03-10 12:03:07 +0100

    Update

--- Zafiro.Core/TreeExtensions.cs
+++ Zafiro.Core/TreeExtensions.cs
@@ -6,8 +6,8 @@
 
     public class TreeExtensions
     {
-        private static IObservable<T> ParentAttached<T>(object targetInstance,
-            IObservable<ParentChildRelationship> childAttached) where T : class
+        public static IObservable<T> ParentAttached<T>(object targetInstance,
+            IObservable<Association> childAttached) where T : class
         {
             ISubject<T> subject = new Subject<T>();
             IDisposable subs = null;
