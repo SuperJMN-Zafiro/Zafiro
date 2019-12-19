27e5b44f64990aaadec4616e969cb256f606e80c
José Manuel Nieto Sánchez <superjmn@outlook.com>
2019-12-20 00:51:00 +0100

    New Converters

---
+++ Source/Zafiro.Uwp/Converters/TypeToStringConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Zafiro.Uwp.Converters
+{
+    public class TypeToStringConverter : List<IValueConverter>, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Type type)
+            {
+                return type.Name;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
