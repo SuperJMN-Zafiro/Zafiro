6b0d08fd4bf0335eda83ac56f8eac7dae754b68f
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-03-19 22:57:40 +0100

    Added BytesToImageSourceConverter

---
+++ Source/Zafiro.UI.Infrastructure.Uno/BytesToImageSourceConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Zafiro.UI.Infrastructure.Uno
+{
+    public class BytesToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(value is byte[] bytes))
+                return null;
+
+            var image = new BitmapImage();
+
+#if WINDOWS_UWP
+            using (var stream = new InMemoryRandomAccessStream())
+            {
+              image.SetSource(stream);
+              var s = stream.AsStreamForWrite();
+              s.Write(bytes, 0, bytes.Length);
+            }
+#else
+            using (Stream stream = new MemoryStream(bytes))
+            {
+                image.SetSource(stream);
+            }
+#endif
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotSupportedException();
+    }
+}
