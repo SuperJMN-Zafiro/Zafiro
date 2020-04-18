06bf049d7a9221554dd049d982c3f6d948822c6d
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-04-18 16:17:25 +0200

    Added Filesystem

---
+++ Source/Zafiro.Core/FileSystem/DirectorySwitch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Serilog;
+
+namespace Zafiro.Core.FileSystem
+{
+    public class DirectorySwitch : IDisposable
+    {
+        private readonly IFileSystemOperations operations;
+        private readonly string oldDirectory;
+
+        public DirectorySwitch(IFileSystemOperations operations, string directory)
+        {
+            this.operations = operations;
+            Log.Debug($"Switching to {directory}");
+
+            oldDirectory = operations.WorkingDirectory;
+            operations.WorkingDirectory = directory;
+        }
+
+        public void Dispose()
+        {
+            Log.Debug($"Returning to {oldDirectory}");
+            operations.WorkingDirectory = oldDirectory;
+        }
+    }
+}
