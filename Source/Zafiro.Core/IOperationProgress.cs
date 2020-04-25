6ce811d9912f4071a013664448ea78e7c59dac56
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-04-25 20:21:46 +0200

    Downloader and ZafiroFile

---
+++ Source/Zafiro.Core/IOperationProgress.cs
@@ -0,0 +1,10 @@
+﻿using System.Reactive.Subjects;
+
+namespace Zafiro.Core
+{
+    public interface IOperationProgress
+    {
+        ISubject<double> Percentage { get; set; }
+        ISubject<long> Value { get; set; }
+    }
+}
