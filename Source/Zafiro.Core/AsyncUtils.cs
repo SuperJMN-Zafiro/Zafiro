2ccfbfe11536d1f0b0512aa063f19e262cee31de
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-09-02 17:09:20 +0200

    AsyncUtils

---
+++ Source/Zafiro.Core/AsyncUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Zafiro.Core.Mixins
+{
+    public static class AsyncUtils
+    {
+        public static async Task<IEnumerable<TResult>> AsyncSelect<TInput, TResult>(this IEnumerable<TInput> items,
+            Func<TInput, Task<TResult>> selector)
+        {
+            var results = new List<TResult>();
+
+            foreach (var item in items)
+            {
+                results.Add(await selector(item));
+            }
+
+            return results;
+        }
+    }
+}
