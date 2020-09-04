9108409d9543c0ad3f89485dc567682ee7362f20
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-09-04 13:38:24 +0200

    Lines

---
+++ Source/Zafiro.Core/Mixins/StringMixin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Zafiro.Core.Mixins
+{
+    public static class StringMixin
+    {
+        public static IEnumerable<string> Lines(this string s)
+        {
+            using var tr = new StringReader(s);
+            while (tr.ReadLine() is { } l)
+            {
+                yield return l;
+            }
+        }
+    }
+}
