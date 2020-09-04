75a73ae2877ae40a3817e5bcd509adc83053a877
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-09-05 01:07:46 +0200

    AsyncExtensions

---
+++ Source/Zafiro.Core/Patterns/AsyncExtensions.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using Optional.Unsafe;
+
+namespace Zafiro.Core.Patterns
+{
+    public static class AsyncExtensions
+    {
+        public static async Task<Either<TLeft, TRight>> RightTask<TLeft, TRight>(this Either<TLeft, Task<TRight>> either) where TLeft : new()
+        {
+            return await either.Right.Match(async task =>
+            {
+                var right = await task;
+                return new Either<TLeft, TRight>(right);
+            }, () => Task.FromResult(new Either<TLeft, TRight>(either.Left.ValueOrDefault())));
+        }
+
+        public static async Task<Either<TLeft, TRight>> RightTask<TLeft, TRight>(this Either<TLeft, Task<Either<TLeft, TRight>>> either)
+        {
+            return await either.Right.Match(async task => await task, () => Task.FromResult(new Either<TLeft, TRight>(either.Left.ValueOrDefault())));
+        }
+    }
+}
