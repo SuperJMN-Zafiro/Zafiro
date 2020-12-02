3e31005d6277356c33089aee2fe4a75030bc5f79
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-12-02 23:41:15 +0100

    Attempt to fix Zafiro.Wpf nuget packager build.

--- Source/Zafiro.Wpf/Services/PopupWindow.xaml.cs
+++ Source/Zafiro.Wpf/Services/PopupWindow.xaml.cs
@@ -1,28 +1,13 @@
-﻿using System.Threading.Tasks;
-using Zafiro.Core;
-
-namespace Zafiro.Wpf.Services
+﻿namespace Zafiro.Wpf.Services
 {
     /// <summary>
     /// Interaction logic for DialogWindow.xaml
     /// </summary>
-    public partial class PopupWindow : IPopup
+    public partial class PopupWindow
     {
         public PopupWindow()
         {
             InitializeComponent();
         }
-
-        public new Task Show()
-        {
-            return this.ShowDialogAsync();
-        }
-
-        public void SetContext(object o)
-        {
-            DataContext = o;
-        }
-
-        public object Object => this.Content;
     }
 }
