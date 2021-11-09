a1d575c1bd764320a413b545847d965e9ae8a0d9
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-11-09 13:06:59 +0100

    Upgrade to .NET 6

--- Source/Zafiro.Reflection.Tests/ReflectedCollectionTests.cs
+++ Source/Zafiro.Reflection.Tests/ReflectedCollectionTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -30,7 +31,7 @@ namespace Zafiro.Reflection.Tests
             
             var child = new Child();
             sut.Add(child);
-            sut.Items.Should().Contain(child);
+            sut.Items.Cast<object>().Should().Contain(child);
         }
 
         [Fact]
