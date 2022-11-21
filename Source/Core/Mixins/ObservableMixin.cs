02fb25c53d5ed9c1ab52977ba0b217ecbbb716f6
José Manuel Nieto <superjmn@outlook.com>
2022-11-21 17:16:54 +0100

    Add new methods to mixins

--- Source/Core/Mixins/ObservableMixin.cs
+++ Source/Core/Mixins/ObservableMixin.cs
@@ -1,16 +1,56 @@
 ﻿using System;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
 
-namespace Zafiro.Core.Mixins
+namespace Zafiro.Core.Mixins;
+
+public static class ObservableMixin
 {
-    public static class ObservableMixin
+    public static IObservable<TSource> Using<TSource, TResource>(
+        Func<Task<TResource>> resourceFactoryAsync,
+        Func<TResource, IObservable<TSource>> observableFactory)
+        where TResource : IDisposable
     {
-        public static IObservable<TSource> Using<TSource, TResource>(
-            Func<Task<TResource>> resourceFactoryAsync,
-            Func<TResource, IObservable<TSource>> observableFactory)
-            where TResource : IDisposable =>
-            Observable.FromAsync(resourceFactoryAsync).SelectMany(
-                resource => Observable.Using(() => resource, observableFactory));
+        return Observable.FromAsync(resourceFactoryAsync).SelectMany(
+            resource => Observable.Using(() => resource, observableFactory));
+    }
+
+    public static IObservable<Unit> ToSignal<T>(this IObservable<T> source)
+    {
+        return source.Select(_ => Unit.Default);
+    }
+
+    public static IObservable<T> ReplayLastActive<T>(this IObservable<T> observable)
+    {
+        return observable.Replay(1).RefCount();
+    }
+
+    public static IObservable<bool> Not(this IObservable<bool> self)
+    {
+        return self.Select(b => !b);
+    }
+
+    public static IObservable<T> WhereSuccess<T>(this IObservable<Result<T>> self)
+    {
+        return self.Where(a => a.IsSuccess)
+            .Select(x => x.Value);
+    }
+
+    public static IObservable<bool> IsSuccess<T>(this IObservable<Result<T>> self)
+    {
+        return self
+            .Select(a => a.IsSuccess);
+    }
+
+    public static IObservable<string> WhereNotEmpty(this IObservable<string> self)
+    {
+        return self.Where(s => !string.IsNullOrWhiteSpace(s));
+    }
+
+    public static IObservable<bool> SelectNotEmpty(this IObservable<string> self)
+    {
+        return self.Select(s => !string.IsNullOrWhiteSpace(s));
     }
 }
--- Source/Core/Mixins/StringMixin.cs
+++ Source/Core/Mixins/StringMixin.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -94,6 +95,21 @@ namespace Zafiro.Core.Mixins
             {
                 yield return l;
             }
+        }
+
+        public static string JoinWithLines<T>(this IEnumerable<T> items)
+        {
+            return Join(items, Environment.NewLine);
+        }
+
+        public static string JoinWithCommas<T>(this IEnumerable<T> items)
+        {
+            return Join(items, ", ");
+        }
+
+        public static string Join<T>(this IEnumerable<T> items, string separator)
+        {
+            return string.Join(separator, items.Select(x => x?.ToString()));
         }
     }
 }
