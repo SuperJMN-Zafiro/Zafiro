5c2b3c5b68fcea7f9b3106f8c8010aa9be52fe5e
José Manuel Nieto <superjmn@outlook.com>
2023-03-11 01:14:53 +0100

    Vamos muy a tope

---
+++ Source/FileSystem/ZafiroPathMixin.cs
@@ -0,0 +1,20 @@
+﻿namespace Zafiro.FileSystem;
+
+public static class ZafiroPathMixin
+{
+    public static string NameWithoutExtension(this ZafiroPath path)
+    {
+        var last = path.RouteFragments.Last();
+        var lastIndex = last.LastIndexOf('.');
+
+        return lastIndex < 0 ? last : last[..lastIndex];
+    }
+
+    public static string Extension(this ZafiroPath path)
+    {
+        var last = path.RouteFragments.Last();
+        var lastIndex = last.LastIndexOf('.');
+
+        return lastIndex < 0 ? last : last[(lastIndex+1)..];
+    }
+}
