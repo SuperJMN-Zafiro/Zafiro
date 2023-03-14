d2755f110cb22e8ba0d192fe1ee770b4ff1b88f8
José Manuel Nieto <superjmn@outlook.com>
2023-03-15 00:16:23 +0100

    Extend FunctionaMixin

--- Source/Core/Mixins/FunctionalMixin.cs
+++ Source/Core/Mixins/FunctionalMixin.cs
@@ -9,22 +9,14 @@ public static class FunctionalMixin
 {
     public static IObservable<Unit> WhereSuccess(this IObservable<Result> self) => self.Where(a => a.IsSuccess).ToSignal();
 
+    public static IObservable<T> WhereSuccess<T>(this IObservable<Result<T>> self) => self.Where(a => a.IsSuccess)
+        .Select(x => x.Value);
+
     public static IObservable<string> WhereFailure(this IObservable<Result> self) => self.Where(a => a.IsFailure).Select(x => x.Error);
 
-    public static IObservable<T> WhereSuccess<T>(this IObservable<Result<T>> self)
-    {
-        return self.Where(a => a.IsSuccess)
-            .Select(x => x.Value);
-    }
+    public static IObservable<string> WhereFailure<T>(this IObservable<Result<T>> self) => self.Where(a => a.IsFailure).Select(x => x.Error);
 
-    public static IObservable<bool> IsSuccess<T>(this IObservable<Result<T>> self)
-    {
-        return self
-            .Select(a => a.IsSuccess);
-    }
+    public static IObservable<bool> IsSuccess<T>(this IObservable<Result<T>> self) => self.Select(a => a.IsSuccess);
 
-    public static IObservable<T> Values<T>(this IObservable<Maybe<T>> self)
-    {
-        return self.Where(x => x.HasValue).Select(x => x.Value);
-    }
+    public static IObservable<T> Values<T>(this IObservable<Maybe<T>> self) => self.Where(x => x.HasValue).Select(x => x.Value);
 }
