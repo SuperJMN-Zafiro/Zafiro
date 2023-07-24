975e7253c5e14fc2e339bee19c0e8e399c0069e6
José Manuel Nieto <superjmn@outlook.com>
2023-07-24 13:33:34 +0200

    Add RelativeProgress

---
+++ Source/Core/ProgressReporting/RelativeProgress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class RelativeProgress<T>
+{
+    public RelativeProgress(T total, T value)
+    {
+        Total = total;
+        Value = value;
+    }
+
+    public T Total { get; }
+    public T Value { get; }
+    public double Proportion => Convert.ToDouble(Value) / Convert.ToDouble(Total);
+}
