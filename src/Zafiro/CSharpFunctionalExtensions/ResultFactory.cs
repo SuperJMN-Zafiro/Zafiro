93b61285a9bae5f385ca280cd3a371dabe41694a
José Manuel Nieto <superjmn@outlook.com>
2023-08-06 18:01:31 +0200

    A little bunch

---
+++ src/Zafiro/CSharpFunctionalExtensions/ResultFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CSharpFunctionalExtensions;
+
+namespace Zafiro.CSharpFunctionalExtensions;
+
+public static class ResultFactory
+{
+    public static Result<K> Combine<T, Q, K>(this Result<T> one, Result<Q> another, Func<T, Q, K> combineFunction)
+    {
+        return one.Bind(x => another.Map(y => combineFunction(x, y)));
+    }
+}
