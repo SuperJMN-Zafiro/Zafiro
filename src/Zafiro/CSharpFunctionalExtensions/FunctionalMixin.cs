56e9adc4f8d3249d5286f73e6c1a8f89c0f70bf8
José Manuel Nieto <superjmn@outlook.com>
2023-10-17 17:20:12 +0200

    Formatting

--- src/Zafiro/CSharpFunctionalExtensions/FunctionalMixin.cs
+++ src/Zafiro/CSharpFunctionalExtensions/FunctionalMixin.cs
@@ -5,7 +5,6 @@ using System.Reactive;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
-using DynamicData;
 using JetBrains.Annotations;
 using Zafiro.Mixins;
 
@@ -14,81 +13,40 @@ namespace Zafiro.CSharpFunctionalExtensions;
 [PublicAPI]
 public static class FunctionalMixin
 {
-    public static IObservable<Unit> Successes(this IObservable<Result> self)
-    {
-        return self.Where(a => a.IsSuccess).ToSignal();
-    }
+    public static IObservable<Unit> Successes(this IObservable<Result> self) => self.Where(a => a.IsSuccess).ToSignal();
 
-    public static IObservable<T> Successes<T>(this IObservable<Result<T>> self)
-    {
-        return self.Where(a => a.IsSuccess).Select(x => x.Value);
-    }
+    public static IObservable<T> Successes<T>(this IObservable<Result<T>> self) => self.Where(a => a.IsSuccess).Select(x => x.Value);
 
-    public static IObservable<bool> IsSuccess<T>(this IObservable<Result<T>> self)
-    {
-        return self.Select(a => a.IsSuccess);
-    }
+    public static IObservable<bool> IsSuccess<T>(this IObservable<Result<T>> self) => self.Select(a => a.IsSuccess);
 
-    public static IObservable<bool> IsSuccess(this IObservable<Result> self)
-    {
-        return self.Select(a => a.IsSuccess);
-    }
+    public static IObservable<bool> IsSuccess(this IObservable<Result> self) => self.Select(a => a.IsSuccess);
 
-    public static IObservable<bool> IsFailure(this IObservable<Result> self)
-    {
-        return self.Select(a => a.IsFailure);
-    }
+    public static IObservable<bool> IsFailure(this IObservable<Result> self) => self.Select(a => a.IsFailure);
 
-    public static IObservable<string> Failures(this IObservable<Result> self)
-    {
-        return self.Where(a => a.IsFailure).Select(x => x.Error);
-    }
+    public static IObservable<string> Failures(this IObservable<Result> self) => self.Where(a => a.IsFailure).Select(x => x.Error);
 
-    public static IObservable<string> Failures<T>(this IObservable<Result<T>> self)
-    {
-        return self.Where(a => a.IsFailure).Select(x => x.Error);
-    }
+    public static IObservable<string> Failures<T>(this IObservable<Result<T>> self) => self.Where(a => a.IsFailure).Select(x => x.Error);
 
-    public static IObservable<T> Values<T>(this IObservable<Maybe<T>> self)
-    {
-        return self.Where(x => x.HasValue).Select(x => x.Value);
-    }
+    public static IObservable<T> Values<T>(this IObservable<Maybe<T>> self) => self.Where(x => x.HasValue).Select(x => x.Value);
 
-    public static IEnumerable<T> Values<T>(this IEnumerable<Maybe<T>> self)
-    {
-        return self.Where(x => x.HasValue).Select(x => x.Value);
-    }
+    public static IEnumerable<T> Values<T>(this IEnumerable<Maybe<T>> self) => self.Where(x => x.HasValue).Select(x => x.Value);
 
-    public static IEnumerable<string> Failures(this IEnumerable<Result> self)
-    {
-        return self.Where(a => a.IsFailure).Select(x => x.Error);
-    }
+    public static IEnumerable<string> Failures(this IEnumerable<Result> self) => self.Where(a => a.IsFailure).Select(x => x.Error);
 
-    public static IEnumerable<string> Failures<T>(this IEnumerable<Result<T>> self)
-    {
-        return self.Where(a => a.IsFailure).Select(x => x.Error);
-    }
+    public static IEnumerable<string> Failures<T>(this IEnumerable<Result<T>> self) => self.Where(a => a.IsFailure).Select(x => x.Error);
 
-    public static IEnumerable<T> Successes<T>(this IEnumerable<Result<T>> self)
-    {
-        return self.Where(a => a.IsSuccess)
-            .Select(x => x.Value);
-    }
+    public static IEnumerable<T> Successes<T>(this IEnumerable<Result<T>> self) => self.Where(a => a.IsSuccess)
+        .Select(x => x.Value);
 
-    public static IEnumerable<string> NotNullOrEmpty(this IEnumerable<string> self)
-    {
-        return self.Where(s => !string.IsNullOrWhiteSpace(s));
-    }
+    public static IEnumerable<string> NotNullOrEmpty(this IEnumerable<string> self) => self.Where(s => !string.IsNullOrWhiteSpace(s));
 
     [Obsolete("You should not need this")]
-    public static IObservable<Unit> Empties<T>(this IObservable<Maybe<T>> self)
-    {
-        return self.Where(x => !x.HasValue).Select(_ => Unit.Default);
-    }
+    public static IObservable<Unit> Empties<T>(this IObservable<Maybe<T>> self) => self.Where(x => !x.HasValue).Select(_ => Unit.Default);
 
     public static bool AnyEmpty<T>(this IEnumerable<Maybe<T>> self)
     {
-        return self.Any(x => x.HasNoValue);
+        if (self.Any(x => x.HasNoValue)) return true;
+        return false;
     }
 
     public static Maybe<TResult> Combine<T, TResult>(this IList<Maybe<T>> values, Func<IEnumerable<T>, TResult> combinerFunc)
@@ -111,23 +69,14 @@ public static class FunctionalMixin
         return Maybe.From(result.Value);
     }
 
-    public static async Task<Maybe<T>> AsMaybe<T>(this Task<Result<T>> resultTask)
-    {
-        return (await resultTask).AsMaybe();
-    }
+    public static async Task<Maybe<T>> AsMaybe<T>(this Task<Result<T>> resultTask) => (await resultTask).AsMaybe();
 
-    public static Result<TDestination> Cast<TSource, TDestination>(this Result<TSource> source, Func<TSource, TDestination> conversionFactory)
-    {
-        return source.Map(conversionFactory);
-    }
+    public static Result<TDestination> Cast<TSource, TDestination>(this Result<TSource> source, Func<TSource, TDestination> conversionFactory) => source.Map(conversionFactory);
 
-    public static Task<Result<TDestination>> Cast<TSource, TDestination>(this Task<Result<TSource>> source, Func<TSource, TDestination> conversionFactory)
-    {
-        return source.Map(conversionFactory);
-    }
+    public static Task<Result<TDestination>> Cast<TSource, TDestination>(this Task<Result<TSource>> source, Func<TSource, TDestination> conversionFactory) => source.Map(conversionFactory);
 
     public static Task<Result<Maybe<TResult>>> Bind<TFirst, TResult>(
-        this Task<Result<Maybe<TFirst>>> task, 
+        this Task<Result<Maybe<TFirst>>> task,
         Func<TFirst, Task<Result<Maybe<TResult>>>> selector)
     {
         return task.Bind(maybe => maybe.Match(f => selector(f), () => Task.FromResult(Result.Success(Maybe<TResult>.None))));
@@ -135,7 +84,7 @@ public static class FunctionalMixin
 
     // TODO: Test this
     public static Result<Maybe<TResult>> Bind<TFirst, TResult>(
-        this Result<Maybe<TFirst>> task, 
+        this Result<Maybe<TFirst>> task,
         Func<TFirst, Result<Maybe<TResult>>> selector)
     {
         return task.Bind(maybe => maybe.Match(f => selector(f), () => Result.Success(Maybe<TResult>.None)));
