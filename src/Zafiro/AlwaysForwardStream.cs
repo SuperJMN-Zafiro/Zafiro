95cad161e021b716be125000e45aa67fc2ef3c14
José Manuel Nieto <superjmn@outlook.com>
2024-03-16 10:43:05 +0100

    Fix Android (#35)

---
+++ src/Zafiro/AlwaysForwardStream.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace Zafiro;
+
+public class AlwaysForwardStream : Stream
+{
+    private readonly Stream inner;
+    private long position;
+
+    public AlwaysForwardStream(Stream inner, long length)
+    {
+        this.inner = inner;
+        Length = length;
+    }
+
+    public override bool CanRead => inner.CanRead;
+
+    public override bool CanSeek => inner.CanSeek;
+
+    public override bool CanWrite => inner.CanWrite;
+
+    public override long Length { get; }
+
+    public override long Position
+    {
+        get => position;
+        set => inner.Position = value;
+    }
+
+    public override void Flush()
+    {
+        inner.Flush();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        inner.Dispose();
+        base.Dispose(disposing);
+    }
+
+    public override ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return inner.DisposeAsync();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        var read = inner.Read(buffer, offset, count);
+        position += read;
+        return read;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return inner.Seek(offset, origin);
+    }
+
+    public override void SetLength(long value)
+    {
+        inner.SetLength(value);
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        inner.Write(buffer, offset, count);
+        position += count;
+    }
+
+    public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    {
+        var readAsync = base.ReadAsync(buffer, offset, count, cancellationToken);
+        position += count;
+        return readAsync;
+    }
+
+    public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    {
+        await base.WriteAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
+        position += count;
+    }
+
+    public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = new())
+    {
+        var readAsync = await base.ReadAsync(buffer, cancellationToken).ConfigureAwait(false);
+        position += readAsync;
+        return readAsync;
+    }
+
+    public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = new())
+    {
+        await base.WriteAsync(buffer, cancellationToken).ConfigureAwait(false);
+        position += buffer.Length;
+    }
+}
