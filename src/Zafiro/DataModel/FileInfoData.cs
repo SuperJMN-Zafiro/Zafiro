3f2528e3b1c7734a131df87cb6a8d669f50ee900
José Manuel Nieto <superjmn@outlook.com>
2024-05-10 12:35:43 +0200

    Big refactor

---
+++ src/Zafiro/DataModel/FileInfoData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Abstractions;
+using System.Reactive.Linq;
+using Zafiro.Reactive;
+
+namespace Zafiro.DataModel;
+
+public class FileInfoData : IData
+{
+    public FileInfoData(IFileInfo file)
+    {
+        Bytes = Observable.Using(file.OpenRead, stream => stream.ToObservableChunked());
+        Length = file.Length;
+    }
+
+    public long Length { get; }
+
+    public IObservable<byte[]> Bytes { get; }
+}
---
+++ src/Zafiro/DataModel/StringData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using Zafiro.Mixins;
+
+namespace Zafiro.DataModel;
+
+public class StringData(string content, Encoding encoding) : IData
+{
+    public StringData(string content) : this(content, Encoding.Default)
+    {
+    }
+
+    public string Content { get; } = content;
+
+    public IObservable<byte[]> Bytes { get; } = content.ToBytes(encoding).ToObservable().Buffer(1024).Select(list => list.ToArray());
+    public long Length { get; } = content.Length;
+
+    public static implicit operator StringData(string content) => new(content, Encoding.Default);
+}
