37d95ce5266989dd65957e505616bd322149fd0b
José Manuel Nieto <superjmn@outlook.com>
2024-11-05 09:56:19 +0100

    Mejoras

--- src/Zafiro/DataAnalysis/Clustering/ClusterNode.cs
+++ src/Zafiro/DataAnalysis/Clustering/ClusterNode.cs
@@ -1,40 +1,94 @@
-﻿namespace Zafiro.DataAnalysis.Clustering;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public interface ICluster
+namespace Zafiro.DataAnalysis.Clustering.Untyped;
+
+public class ClusterNode : ICluster
 {
-    public object Item { get; }
-}
+    public ClusterNode? Parent { get; init; }
+    public ClusterNode? Left { get; init; }
+    public ClusterNode? Right { get; init; }
 
-public abstract class ClusterNode<T> : ICluster
-{
-    public object Item { get; }
-}
+    ICluster? ICluster.Parent => Parent;
+    ICluster? ICluster.Left => Left;
+    ICluster? ICluster.Right => Right;
+    IEnumerable<ICluster> ICluster.Children => Children;
 
-public class LeafNode<T> : ClusterNode<T> where T : class
-{
-    public T Item { get; }
+    public double MergeDistance { get; init; }
+    public object? Item { get; init; }
 
-    public LeafNode(T item)
+    public IEnumerable<ClusterNode> Children
+    {
+        get
+        {
+            if (Left != null) yield return Left;
+            if (Right != null) yield return Right;
+        }
+    }
+
+    // Constructor para nodos hoja
+    private ClusterNode(object? item, ClusterNode? parent = null)
     {
         Item = item;
+        Parent = parent;
+        MergeDistance = 0;
+        Left = null;
+        Right = null;
     }
 
-    public override string? ToString()
-    {
-        return Item.ToString();
-    }
-}
-
-public class InternalNode<T> : ClusterNode<T>
-{
-    public ClusterNode<T> Left { get; }
-    public ClusterNode<T> Right { get; }
-    public double MergeDistance { get; }
-
-    public InternalNode(ClusterNode<T> left, ClusterNode<T> right, double mergeDistance)
+    // Constructor para nodos internos
+    private ClusterNode(ClusterNode left, ClusterNode right, double mergeDistance, ClusterNode? parent = null)
     {
         Left = left;
         Right = right;
         MergeDistance = mergeDistance;
+        Parent = parent;
+        Item = null;
+
+        // Establecer el padre en los nodos hijos si aún no lo tienen
+        if (Left != null && Left.Parent == null)
+        {
+            Left = new ClusterNode(Left.Item, this)
+            {
+                Left = Left.Left,
+                Right = Left.Right,
+                MergeDistance = Left.MergeDistance
+            };
+        }
+
+        if (Right != null && Right.Parent == null)
+        {
+            Right = new ClusterNode(Right.Item, this)
+            {
+                Left = Right.Left,
+                Right = Right.Right,
+                MergeDistance = Right.MergeDistance
+            };
+        }
+    }
+
+    public static ClusterNode Create<T>(ClusterNode<T> node, ClusterNode parent = null) where T : class
+    {
+        if (node is LeafNode<T> leafNode)
+        {
+            // Crear un nodo hoja del ViewModel
+            return new ClusterNode(leafNode.Item, parent);
+        }
+        else if (node is InternalNode<T> internalNode)
+        {
+            // Construir recursivamente los nodos hijo
+            var leftViewModel = Create(internalNode.Left);
+            var rightViewModel = Create(internalNode.Right);
+
+            // Crear un nodo interno del ViewModel
+            var internalViewModel = new ClusterNode(leftViewModel, rightViewModel, internalNode.MergeDistance, parent);
+
+            return internalViewModel;
+        }
+        else
+        {
+            throw new InvalidOperationException("Tipo de nodo desconocido");
+        }
     }
 }
