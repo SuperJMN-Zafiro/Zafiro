773949ee17f9bbf01aa63dc973deec333f46e8e9
José Manuel Nieto (SuperJMN) <superjmn@outlook.com>
2024-11-21 15:39:39 +0100

    Improvements

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using CSharpFunctionalExtensions;
 using Nuke.Common;
-using Nuke.Common.CI.AzurePipelines;
 using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.GitVersion;
-using Zafiro.CSharpFunctionalExtensions;
 using Zafiro.Deployment;
-using Zafiro.Misc;
 
-
-[AzurePipelines(AzurePipelinesImage.WindowsLatest, ImportSecrets = new[] { nameof(NuGetApiKey) }, AutoGenerate = false)]
 class Build : NukeBuild
 {
     [Parameter] [Secret] readonly string NuGetApiKey;
@@ -20,23 +16,22 @@ class Build : NukeBuild
     [GitVersion] readonly GitVersion GitVersion;
     [GitRepository] readonly GitRepository Repository;
    
-
     Target Publish => td => td
         .Requires(() => NuGetApiKey)
         .OnlyWhenStatic(() => Repository.IsOnMainOrMasterBranch() || Force)
         .Executes(async () =>
         {
-            var packableProjects = Solution.AllProjects
-                .Where(x => x.GetProperty<bool>("IsPackable"))
-                .Where(x => !x.Path.ToString().Contains("Test", StringComparison.InvariantCultureIgnoreCase) && !x.Path.ToString().Contains("Sample", StringComparison.InvariantCultureIgnoreCase)).ToList();
+            var version = GitVersion.NuGetVersion;
             
-            await packableProjects
-                .Select(project => Packager.Instance.CreateForNuGet(project, GitVersion.NuGetVersion).LogInfo($"Packing {project}"))
-                .CombineSequentially()
-                .MapEach(file => Publisher.Instance.ToNuGet(file, NuGetApiKey).LogInfo($"Pushing package {file}"))
-                .CombineSequentially()
-                .TapError(failed => Assert.Fail(failed));
+            await Deployer.Instance.PublishPackages(PackableProjects, version, NuGetApiKey)
+                .TapError(error => Assert.Fail(error.ToString()));
         });
-    
+
+    IEnumerable<string> PackableProjects =>
+        Solution.AllProjects
+            .Where(x => x.GetProperty<bool>("IsPackable"))
+            .Where(x => !(x.Path.ToString().Contains("Test", StringComparison.InvariantCultureIgnoreCase) || x.Path.ToString().Contains("Sample", StringComparison.InvariantCultureIgnoreCase)))
+            .Select(x => x.Path.ToString());
+
     public static int Main() => Execute<Build>(x => x.Publish);
 }
