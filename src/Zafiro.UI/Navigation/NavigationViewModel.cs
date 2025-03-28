32a2f7796758d8e80f7cd42df410eed7012a92a8
José Manuel Nieto <superjmn@outlook.com>
2025-03-28 23:33:01 +0100

    Add NavigationViewModel

---
+++ src/Zafiro.UI/Navigation/NavigationViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Reactive;
+using ReactiveUI;
+
+namespace Zafiro.UI.Navigation;
+
+public class NavigationViewModel : ReactiveObject
+{
+    public NavigationViewModel(INavigator navigator, Func<object> viewModel)
+    {
+        Navigator = navigator;
+        Create = ReactiveCommand.CreateFromTask(() => Navigator.Go(viewModel));
+        Create.Execute().Subscribe();
+    }
+
+    public INavigator Navigator { get; }
+   
+    public ReactiveCommand<Unit, Unit> Create { get; }
+}
