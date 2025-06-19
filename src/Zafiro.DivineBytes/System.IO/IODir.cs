f5a671b8061050bc908f4bbe517fb881f783be21
SuperJMN <superjmn@outlook.com>
2025-06-20 00:45:15 +0200

    Add Android

--- src/Zafiro.DivineBytes/System.IO/IODir.cs
+++ src/Zafiro.DivineBytes/System.IO/IODir.cs
@@ -1,5 +1,4 @@
 using System.IO.Abstractions;
-using MsixPackaging.Tests.Helpers;
 
 namespace Zafiro.DivineBytes.System.IO;
 
--- test/Zafiro.Deployement.New.Tests/Integration/PackagingTests.cs
+++ test/Zafiro.Deployement.New.Tests/Integration/PackagingTests.cs
@@ -1,11 +1,17 @@
+using System.Text;
 using CSharpFunctionalExtensions;
+using FluentAssertions;
 using Serilog;
+using Xunit.Abstractions;
 using Zafiro.Deployment.Core;
 using Zafiro.Deployment.New;
+using Zafiro.Deployment.New.Platforms;
+using Zafiro.DivineBytes;
+using File = System.IO.File;
 
 namespace Zafiro.Deployement.New.Tests.Integration;
 
-public class PackagingTests
+public class PackagingTests(ITestOutputHelper outputHelper)
 {
     [Fact]
     public async Task TestWindows()
@@ -20,5 +26,31 @@ public class PackagingTests
         
         var result = await new SuperPackager(dotnet, Maybe<ILogger>.None)
             .CreateForWindows("/mnt/fast/Repos/SuperJMN-Zafiro/Zafiro.Avalonia/samples/TestApp/TestApp.Desktop/TestApp.Desktop.csproj", options);
+    }
+    
+    [Fact]
+    public async Task Test_android()
+    {
+        var logger = new LoggerConfiguration().WriteTo.TestOutput(outputHelper).CreateLogger();
+        
+        var dotnet = new Dotnet(new Command(logger), logger);
+        
+        var store = ByteSource.FromBytes(await File.ReadAllBytesAsync("Integration/test.keystore.b64"));
+        
+        var options = new AndroidDeployment.DeploymentOptions
+        {
+            AndroidSigningKeyStore = store,
+            ApplicationDisplayVersion = "1.0",
+            ApplicationVersion = 1,
+            SigningKeyAlias = "android",
+            SigningKeyPass = "test1234",
+            SigningStorePass = "test1234",
+        };
+
+        
+        var result = await new SuperPackager(dotnet, logger)
+            .CreateForAndroid("/mnt/fast/Repos/SuperJMN/angor/src/Angor/Avalonia/AngorApp.Android/AngorApp.Android.csproj", options);
+
+        result.Should().Succeed();
     }
 }
