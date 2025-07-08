05f30cf7aa3fcfe57a9a62c6befd45387eb40e50
SuperJMN <superjmn@outlook.com>
2025-07-09 00:32:33 +0200

    Update DefaultMetadataResolver to set default size to 1000 for directory and file metadata

--- src/Zafiro.DivineBytes/Unix/DefaultMetadataResolver.cs
+++ src/Zafiro.DivineBytes/Unix/DefaultMetadataResolver.cs
@@ -4,11 +4,11 @@ public class DefaultMetadataResolver : IMetadataResolver
 {
     public Metadata ResolveDirectory(INamedContainer dir)
     {
-        return new Metadata(Permission.All, 0);
+        return new Metadata(Permission.All, 1000);
     }
 
     public Metadata ResolveFile(INamedByteSource file)
     {
-        return new Metadata(Permission.All, 0);
+        return new Metadata(Permission.All, 1000);
     }
 }
