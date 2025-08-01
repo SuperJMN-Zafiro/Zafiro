a1823f4ffd25d047bca6651cc9a7e927e398e11c
José Manuel Nieto <superjmn@outlook.com>
2025-08-01 18:16:27 +0200

    Make ProceedWithResultWhenValid text parameter optional

--- src/Zafiro.UI/Wizards/Slim/Builder/StepBuilderExtensions.cs
+++ src/Zafiro.UI/Wizards/Slim/Builder/StepBuilderExtensions.cs
@@ -6,7 +6,7 @@ namespace Zafiro.UI.Wizards.Slim.Builder;
 
 public static class StepBuilderExtensions
 {
-    public static WizardBuilder<TResult> ProceedWithResultWhenValid<TPage, TResult>(this StepBuilder<TPage> builder, string? text, Func<TPage, Result<TResult>> nextAction) where TPage : IValidatable
+    public static WizardBuilder<TResult> ProceedWithResultWhenValid<TPage, TResult>(this StepBuilder<TPage> builder, Func<TPage, Result<TResult>> nextAction, string? text = null) where TPage : IValidatable
     {
         return builder.ProceedWith(page => EnhancedCommand.Create(() => nextAction(page), page.IsValid, text));
     }
