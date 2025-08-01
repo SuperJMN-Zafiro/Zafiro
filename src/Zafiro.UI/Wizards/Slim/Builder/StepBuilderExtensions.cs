b6aaa170ce2c8031e27e86531690db0f7e35ceaa
José Manuel Nieto <superjmn@outlook.com>
2025-08-01 18:04:46 +0200

    Rename builder helpers for clarity

--- src/Zafiro.UI/Wizards/Slim/Builder/StepBuilderExtensions.cs
+++ src/Zafiro.UI/Wizards/Slim/Builder/StepBuilderExtensions.cs
@@ -6,9 +6,9 @@ namespace Zafiro.UI.Wizards.Slim.Builder;
 
 public static class StepBuilderExtensions
 {
-    public static WizardBuilder<TResult> WithNextCommandIfValid<TPage, TResult>(this StepBuilder<TPage> builder, string? text, Func<TPage, Result<TResult>> nextAction) where TPage : IValidatable
+    public static WizardBuilder<TResult> ProceedWithResultWhenValid<TPage, TResult>(this StepBuilder<TPage> builder, string? text, Func<TPage, Result<TResult>> nextAction) where TPage : IValidatable
     {
-        return builder.WithNextCommand(page => EnhancedCommand.Create(() => nextAction(page), page.IsValid, text));
+        return builder.ProceedWith(page => EnhancedCommand.Create(() => nextAction(page), page.IsValid, text));
     }
 }
 
