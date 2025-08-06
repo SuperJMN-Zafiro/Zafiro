162ec0eb5114eb442d22b6a1af91eafb9b035881
SuperJMN <superjmn@outlook.com>
2025-08-06 16:52:01 +0200

    Stop caching section content

--- src/Zafiro.UI/Shell/Shell.cs
+++ src/Zafiro.UI/Shell/Shell.cs
@@ -1,12 +1,9 @@
 ﻿using System.Reactive.Linq;
-using JetBrains.Annotations;
 using ReactiveUI.SourceGenerators;
-using Zafiro.Reactive;
 using Zafiro.UI.Navigation.Sections;
 
 namespace Zafiro.UI.Shell;
 
-[PublicAPI]
 public partial class Shell : ReactiveObject, IShell
 {
     [Reactive] private IContentSection? selectedSection;
@@ -14,29 +11,23 @@ public partial class Shell : ReactiveObject, IShell
     public Shell(ShellProperties shellProperties, IEnumerable<ISection> sections)
     {
         Sections = sections;
+        Content = this.WhenAnyObservable(x => x.SelectedSection!.Content);
 
-        Content = this.WhenAnyValue(x => x.SelectedSection)
-            .WhereNotNull()
-            .DistinctUntilChanged()
-            .Select(ret => ret.Content)
-            .Switch()
-            .ReplayLastActive();
-        
         ContentHeader = Content
             .WhereNotNull()
             .Select(section => shellProperties.GetHeader(section))
-            .Switch()
-            .ReplayLastActive();
-        
+            .Switch();
+
         SelectedSection = Sections.OfType<IContentSection>().FirstOrDefault();
         Header = shellProperties.Header;
     }
 
     public IObservable<object?> Content { get; }
+
     public object Header { get; set; }
     public IObservable<object?> ContentHeader { get; }
     public IEnumerable<ISection> Sections { get; }
-    
+
     public void GoToSection(string sectionName)
     {
         SelectedSection = Sections.OfType<IContentSection>().First(x => x.Name == sectionName);
