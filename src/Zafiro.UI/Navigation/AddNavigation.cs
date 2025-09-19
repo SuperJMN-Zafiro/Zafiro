9fd9d09b8740f2208c784d407c3c22f549c91eaa
SuperJMN <superjmn@outlook.com>
2025-09-19 23:44:44 +0200

    Sections react to changes

--- src/Zafiro.UI/Navigation/AddNavigation.cs
+++ src/Zafiro.UI/Navigation/AddNavigation.cs
@@ -7,6 +7,13 @@ namespace Zafiro.UI.Navigation;
 
 public static class AddNavigation
 {
+    public static IServiceCollection AddNavigator(this IServiceCollection serviceCollection, ILogger? logger = null, IScheduler? scheduler = null)
+    {
+        serviceCollection.AddScoped<INavigator>(provider => new Navigator(provider, logger.AsMaybe(), scheduler));
+
+        return serviceCollection;
+    }
+    
     public static IServiceCollection RegisterSections(this IServiceCollection serviceCollection, Action<SectionsBuilder> configure, ILogger? logger = null, IScheduler? scheduler = null)
     {
         serviceCollection.AddScoped<INavigator>(provider => new Navigator(provider, logger.AsMaybe(), scheduler));
--- src/Zafiro.UI/Navigation/Sections/Section.cs
+++ src/Zafiro.UI/Navigation/Sections/Section.cs
@@ -1,8 +1,10 @@
+using ReactiveUI.SourceGenerators;
+
 namespace Zafiro.UI.Navigation.Sections;
 
-public class Section : ReactiveObject, ISection
+public abstract partial class Section : ReactiveObject, ISection
 {
     public bool IsPrimary { get; init; } = true;
-    public bool IsVisible { get; set; } = true;
-    public int SortOrder { get; set; } = 0;
+    [Reactive] private bool isVisible = true;
+    [Reactive] private int sortOrder = 0;
 }
