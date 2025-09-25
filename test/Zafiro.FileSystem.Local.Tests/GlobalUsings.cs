ee600d44d00d5d944c9e80e7f2748a2703c1ae1b
SuperJMN <superjmn@outlook.com>
2025-09-25 16:19:07 +0200

    Clean up

--- test/Zafiro.FileSystem.Local.Tests/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-// Global using directives
-
-global using FluentAssertions;
-global using Xunit;
-global using Directory = Zafiro.FileSystem.Readonly.Directory;
-global using File = Zafiro.FileSystem.Readonly.File;
-global using IFile = Zafiro.FileSystem.Readonly.IFile;
